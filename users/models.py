ccd6804130075fd112aa6ab2b623503f2539fff1
Chi heon Song <hrpp1353@gmail.com>
2021-11-26 16:24:33 +0900

    feat: Add modeling and success migration
    - User, TimeStamp, AccountBook 모델 생성
    - make migration file
    - migrate to mysql

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStamp
+
+
+class User(TimeStamp):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    nick_name = models.CharField(max_length=16)
+    email = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = 'users'
