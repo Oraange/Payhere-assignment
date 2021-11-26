26471a65fd28a583f6e5fee4cedb305910a2a51c
Chi heon Song <hrpp1353@gmail.com>
2021-11-26 16:24:33 +0900

    feat: Add modeling and success migration
    - User, TimeStamp, AccountBook 모델 생성
    - make migration file
    - migrate to mysql

--- account_books/models.py
+++ account_books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from core.models import TimeStamp
+
+
+class AccountBook(TimeStamp):
+    
+    class Type(models.IntegerChoices):
+        INCOME = 1
+        OUTLAY = 2
+    
+    type = models.IntegerField(choices=Type.choices)
+    amount = models.PositiveIntegerField(db_index=True)
+    category = models.CharField(max_length=128)
+    memo = models.TextField()
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'account_books'
