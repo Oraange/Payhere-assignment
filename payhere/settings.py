34f9d9e439de77a311d98e41bba478b4fe3c5ba4
Chi heon Song <hrpp1353@gmail.com>
2021-11-30 20:00:37 +0900

    deploy: Add ec2 port in settings
    - settings 파일에 ec2 포트 설정

--- payhere/settings.py
+++ payhere/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '3.36.59.83', '3.36.59.83:8000']
 
 
 # Application definition
